7d1cc661c0001b0766bac87daee3eef710a43a85
Mubelotix <mubelotix@gmail.com>
2024-01-19 15:51:34 +0100

    Setting up

---
+++ src/env.rs
@@ -0,0 +1,30 @@
+use std::{process::{Command, ExitStatus}, path::Path};
+
+fn run_shell_command(command: impl AsRef<str>) -> Result<String, ExitStatus> {
+    let command = command.as_ref();
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("failed to execute process");
+    if output.status.success() {
+        Ok(String::from_utf8(output.stdout).unwrap())
+    } else {
+        Err(output.status)
+    }
+}
+
+pub fn create_venv() {
+    if Path::new("venv").exists() {
+        return;
+    }
+    run_shell_command("python3 -m venv venv").expect("failed to create venv");
+}
+
+pub fn install_instaloader() {
+    let r = run_shell_command("venv/bin/pip install instaloader --upgrade");
+    if r.is_err() && Path::new("venv/bin/instaloader").exists() {
+        return;
+    }
+    r.expect("failed to install instaloader");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 mod config;
 use config::*;
+mod env;
+use env::*;
 
 fn main() {
+    // Read config
     let (doc, config) = read_config("config.toml");
     println!("{:#?}", config);
 
-    // Read config
+    println!("Setting up...");
 
     // Create venv
+    create_venv();
 
     // Install instaloader
+    install_instaloader();
 
     // For each new post:
 
