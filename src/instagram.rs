198fa9d1a9562bab38c2e9e81c5ef550d28e8ee7
Mubelotix <mubelotix@gmail.com>
2024-01-19 16:37:13 +0100

    Progress bar

--- src/instagram.rs
+++ src/instagram.rs
@@ -1,16 +1,27 @@
 use crate::*;
-use progress_bar::{global, init_progress_bar, enable_eta, inc_progress_bar, print_progress_bar_info, finalize_progress_bar};
+use progress_bar::{
+    enable_eta, finalize_progress_bar, inc_progress_bar, init_progress_bar,
+    print_progress_bar_info, Color, Style, set_progress_bar_action,
+};
 
-fn download(instagram_username: &str) {
-    run_shell_command(format!("venv/bin/instaloader --latest-stamps profiles/latest-stamps.ini --dirname-pattern=profiles/{{profile}} -- profile {instagram_username}")).expect("failed to download");
+fn download(instagram_username: &str) -> Result<(), ()> {
+    run_shell_command(
+        format!(
+            "venv/bin/instaloader --latest-stamps profiles/latest-stamps.ini --dirname-pattern=profiles/{{profile}} -- profile {instagram_username}"
+        ))
+        .map_err(|_| ())?;
+    Ok(())
 }
 
 pub fn download_all(config: &Config) {
     init_progress_bar(config.len());
     enable_eta();
+    set_progress_bar_action("Downloading", Color::Green, Style::Bold);
     for account in config {
-        print_progress_bar_info("Downloading", &format!("@{}@instagram.com", account.instagram), progress_bar::Color::Blue, progress_bar::Style::Normal);
-        download(&account.instagram);
+        print_progress_bar_info("Downloading", &format!("{}", account.instagram), Color::Green, Style::Bold);
+        if download(&account.instagram).is_err() {
+            print_progress_bar_info("Failed", &format!("download for @{}@instagram.com", account.instagram), Color::Red, Style::Bold);
+        }
         inc_progress_bar();
     }
     finalize_progress_bar();
